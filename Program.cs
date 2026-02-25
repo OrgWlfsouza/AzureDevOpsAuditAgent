f81e52b255b06af9f656479e645215656ca990b5
Wagner Souza <wlfsouza@hotmail.com>
2026-02-25 10:27:40 -0300

    Clean up Program.cs by removing comments and duplicates
    Removed unnecessary comments and duplicate service registration.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
 using AzureDevOpsAuditAgent.Class;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// service azure devops - teste
 builder.Services.AddHttpClient<AzureDevOpsService>();
 builder.Services.AddControllers();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
