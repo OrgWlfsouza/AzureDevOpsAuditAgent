5a897833979c4d2d04c946de8ab9df8759821ece
Wagner Souza Git <wlfsouza@hotmail.com>
2026-02-25 10:53:47 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
 using AzureDevOpsAuditAgent.Class;
-using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// service azure devops - teste
 builder.Services.AddHttpClient<AzureDevOpsService>();
 builder.Services.AddControllers();
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
