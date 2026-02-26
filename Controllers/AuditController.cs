7c03449c375d5f63b230739a446340a1db844245
Wagner Souza Git <wlfsouza@hotmail.com>
2026-02-25 21:40:26 -0300

    ajuste no controller para atende swagger

--- Controllers/AuditController.cs
+++ Controllers/AuditController.cs
@@ -13,39 +13,65 @@ public class AuditController : ControllerBase
     }
 
     [HttpGet("projects/count")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProjectCountResponse))]
     public async Task<IActionResult> GetProjectCount()
     {
         var count = await _service.GetProjectCountAsync();
-        return Ok(new { Projects = count });
+        return Ok(new ProjectCountResponse { Projects = count });
     }
 
     [HttpGet("users/count")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserCountResponse))]
     public async Task<IActionResult> GetUserCount()
     {
         var count = await _service.GetUserCountAsync();
-        return Ok(new { Users = count });
+        return Ok(new UserCountResponse { Users = count });
     }
 
     [HttpGet("users/license/{email}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserLicenseResponse))]
     public async Task<IActionResult> GetUserLicense(string email)
     {
         var license = await _service.GetUserLicenseAsync(email);
-        return Ok(new { User = email, License = license });
+        return Ok(new UserLicenseResponse { User = email, License = license });
     }
 
     [HttpGet("projects/{projectId}/admins")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProjectAdminsResponse))]
     public async Task<IActionResult> GetProjectAdministrators(string projectId)
     {
         var admins = await _service.GetProjectAdministratorsAsync(projectId);
-        return Ok(new { Project = projectId, Administrators = admins });
+        return Ok(new ProjectAdminsResponse { Project = projectId, Administrators = admins });
     }
 
     [HttpGet("projects/{projectId}/admins/resolved")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProjectAdminsResponse))]
     public async Task<IActionResult> GetProjectAdministratorsResolved(string projectId)
     {
         var admins = await _service.GetProjectAdministratorsResolvedAsync(projectId);
-        return Ok(new { Project = projectId, Administrators = admins });
+        return Ok(new ProjectAdminsResponse { Project = projectId, Administrators = admins });
     }
 }
 
+// DTOs para enriquecer o Swagger
+public class ProjectCountResponse
+{
+    public int Projects { get; set; }
+}
 
+public class UserCountResponse
+{
+    public int Users { get; set; }
+}
+
+public class UserLicenseResponse
+{
+    public string User { get; set; }
+    public string License { get; set; }
+}
+
+public class ProjectAdminsResponse
+{
+    public string Project { get; set; }
+    public IEnumerable<string> Administrators { get; set; }
+}
